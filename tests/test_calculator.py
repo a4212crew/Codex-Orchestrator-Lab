f9d331feb1dcbbb4de54d069b96246897a236fb6
a4212crew <maximlee4212@gmail.com>
2026-09-21 23:15:32 +1000

    Add subtract operation

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,14 @@
-from src.calculator import add, multiply
+from src.calculator import add, multiply, subtract
 
 
 def test_add():
     assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(3, 5) == -2
+    assert subtract(3, 3) == 0
 
 
 def test_multiply():

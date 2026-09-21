745b27fc67537d050de4935164a97eb819b1b346
a4212crew <maximlee4212@gmail.com>
2026-09-21 22:43:36 +1000

    Add calculator multiply feature

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,12 @@
-from src.calculator import add
+from src.calculator import add, multiply
 
 
 def test_add():
     assert add(2, 3) == 5
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-2, 3) == -6
+    assert multiply(0, 3) == 0
+    assert multiply(1.5, 2) == 3.0
